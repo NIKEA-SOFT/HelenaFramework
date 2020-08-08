c917e181949be87161df22abf7438565d5d0ee98
NIKEA-SOFT <nsoft.sagaonline@gmail.com>
2020-08-08 04:49:25 +0300

    Change module EntryPoint view

--- Helena/Module/ModuleTestA/HFMain.cpp
+++ Helena/Module/ModuleTestA/HFMain.cpp
@@ -1,19 +1,19 @@
-#include "Module.hpp"
+#include <Helena/Common/HFApp.hpp>
+#include <Helena/Module/ModuleTestA/Module.hpp>
 
-using Helena::HFApp;
-using Helena::HF_MODULE_STATE;
-using Helena::ModuleTestA;
-
-HF_API void HFMain(HFApp* pApp, HF_MODULE_STATE state) 
+namespace Helena
 {
-    switch(state)
+    HF_API void HFMain(HFApp* pApp, HF_MODULE_STATE state) 
     {
-        case HF_MODULE_STATE::HF_MODULE_INIT : {   
-            pApp->AddModule<ModuleTestA>();
-        } break;
+        switch(state)
+        {
+            case HF_MODULE_STATE::HF_MODULE_INIT : {   
+                pApp->AddModule<ModuleTestA>();
+            } break;
 
-        case HF_MODULE_STATE::HF_MODULE_FREE : {
-            pApp->RemoveModule<ModuleTestA>();
-        } break;
+            case HF_MODULE_STATE::HF_MODULE_FREE : {
+                pApp->RemoveModule<ModuleTestA>();
+            } break;
+        }
     }
 }
--- Helena/Module/ModuleTestB/HFMain.cpp
+++ Helena/Module/ModuleTestB/HFMain.cpp
@@ -1,19 +1,19 @@
-#include "Module.hpp"
+#include <Helena/Common/HFApp.hpp>
+#include <Helena/Module/ModuleTestB/Module.hpp>
 
-using Helena::HFApp;
-using Helena::HF_MODULE_STATE;
-using Helena::ModuleTestB;
-
-HF_API void HFMain(HFApp* pApp, HF_MODULE_STATE state) 
+namespace Helena
 {
-    switch(state)
+    HF_API void HFMain(HFApp* pApp, HF_MODULE_STATE state) 
     {
-        case HF_MODULE_STATE::HF_MODULE_INIT : {   
-            pApp->AddModule<ModuleTestB>();
-        } break;
+        switch(state)
+        {
+            case HF_MODULE_STATE::HF_MODULE_INIT : {   
+                pApp->AddModule<ModuleTestB>();
+            } break;
 
-        case HF_MODULE_STATE::HF_MODULE_FREE : {
-            pApp->RemoveModule<ModuleTestB>();
-        } break;
+            case HF_MODULE_STATE::HF_MODULE_FREE : {
+                pApp->RemoveModule<ModuleTestB>();
+            } break;
+        }
     }
 }
