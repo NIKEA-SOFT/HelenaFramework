a4101a28eaa3b473fb7e697252a74ee44565a758
NIKEA-SOFT <nsoft.sagaonline@gmail.com>
2021-06-19 06:47:16 +0300

    TODO: TaskManager system

---
+++ Helena/Systems/TaskManager.hpp
@@ -0,0 +1,15 @@
+#ifndef HELENA_SYSTEMS_TASKMANAGER_HPP
+#define HELENA_SYSTEMS_TASKMANAGER_HPP
+
+namespace Helena::Systems
+{
+    // TODO
+    class TaskManager {
+
+
+    };
+}
+
+#include <Helena/Systems/TaskManager.ipp>
+
+#endif // HELENA_SYSTEMS_TASKMANAGER_HPP
