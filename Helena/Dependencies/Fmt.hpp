e8d403e68a63be194a2f941ff29a4eca48c565cd
nsoft.sagaonline@gmail.com <nsoft.sagaonline@gmail.com>
2021-10-17 19:13:06 +0300

    Add support fmt

---
+++ Helena/Dependencies/Fmt.hpp
@@ -0,0 +1,13 @@
+#ifndef HELENA_DEPENDENCIES_FMT_HPP
+#define HELENA_DEPENDENCIES_FMT_HPP
+
+#define FMT_HEADER_ONLY
+#define FMT_HAS_CONSTEVAL
+#define FMT_USE_USER_DEFINED_LITERALS 0
+
+#include <fmt/core.h>
+#include <fmt/format.h>
+#include <fmt/chrono.h>
+#include <fmt/color.h>
+
+#endif // HELENA_DEPENDENCIES_FMT_HPP
