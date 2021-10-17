c38fc37dc529852d8e377544e684de41b164aa49
nsoft.sagaonline@gmail.com <nsoft.sagaonline@gmail.com>
2021-10-17 19:07:02 +0300

    Add SourceLocation impl

---
+++ Helena/Util/SourceLocation.hpp
@@ -0,0 +1,36 @@
+#ifndef HELENA_UTIL_SOURCELOCATION_HPP
+#define HELENA_UTIL_SOURCELOCATION_HPP
+
+#include <cstdint>
+#include <string_view>
+#include <algorithm>
+
+namespace Helena::Util
+{
+    struct SourceLocation
+    {
+        [[nodiscard]] static constexpr std::string_view GetSourceName(const std::string_view file, const std::string_view delimeter) noexcept {
+            const auto it = std::find_first_of(file.crbegin(), file.crend(), delimeter.cbegin(), delimeter.cend());
+            return std::string_view(it == file.crend() ? file.cbegin() : it.base(), file.cend());
+        }
+
+        [[nodiscard]] static consteval auto Create(const std::string_view file, const std::uint_least32_t line) noexcept {
+            return SourceLocation{GetSourceName(file, std::string_view{"\\/"}).data(), line};
+        }
+
+        constexpr SourceLocation(const char* const file, const std::uint_least32_t line) noexcept : m_File { file }, m_Line { line } {}
+
+        [[nodiscard]] constexpr std::string_view GetFile() const noexcept {
+            return m_File;
+        }
+
+        [[nodiscard]] constexpr std::uint_least32_t GetLine() const noexcept {
+            return m_Line;
+        }
+
+        const char* const m_File;
+        const std::uint_least32_t m_Line;
+    };
+}
+
+#endif // HELENA_UTIL_SOURCELOCATION_HPP
