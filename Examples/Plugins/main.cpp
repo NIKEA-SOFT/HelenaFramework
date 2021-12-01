1744fd9d8401d72228a9b01349d5fed80ab66584
NIKEA-SOFT <nsoft.sagaonline@gmail.com>
2021-12-01 20:47:58 +0300

    Update examples and add plugin

---
+++ Examples/Plugins/main.cpp
@@ -0,0 +1,20 @@
+﻿#include <Helena/Systems/PluginManager.hpp>
+
+class GameApplication
+{
+public:
+	GameApplication(std::uint32_t val) : value{val} {}
+	~GameApplication() = default;
+
+	std::uint32_t value{};
+};
+
+HELENA_PLUGIN_API void PluginInit(std::shared_ptr<Helena::Engine::Context> ctx) {
+	Helena::Engine::Context::Initialize(ctx);
+
+	Helena::Engine::RegisterSystem<GameApplication>(100'000);
+}
+
+HELENA_PLUGIN_API void PluginEnd() {
+	Helena::Engine::RemoveSystem<GameApplication>();
+}
