1fd7d82c78e9544082d55a470ae8da33ba7aca9a
NIKEA-SOFT <nsoft.sagaonline@gmail.com>
2021-12-16 16:19:56 +0300

    Tab fix

--- Helena/Util/Format.hpp
+++ Helena/Util/Format.hpp
@@ -2,35 +2,33 @@
 #define HELENA_UTIL_FORMAT_HPP
 
 #include <Helena/Dependencies/Fmt.hpp>
-#include <Helena/Debug/Assert.hpp>
-
-#include <string>
+#include <Helena/Engine/Log.hpp>
 
 namespace Helena::Util
 {
-	template <typename... Args>
-	[[nodiscard]] std::string Format(std::string_view msg, const Args&&... args) noexcept 
-	{
-		try {
-			if(!msg.empty()) {
-				return fmt::vformat(msg, fmt::make_format_args(args...));
-			}
-		} catch(const fmt::format_error&) {
-			HELENA_MSG_EXCEPTION(
-				"\n----------------------------------------\n"
-				"|| Error: format syntax invalid!\n"
-				"|| Format: {}"
-				"\n----------------------------------------", msg);
-		} catch(const std::bad_alloc&) {
-			HELENA_MSG_EXCEPTION(
-				"\n----------------------------------------\n"
-				"|| Error: not enough memory for alloc\n"
-				"|| Format: {}"
-				"\n----------------------------------------", msg);
-		}
+    template <typename... Args>
+    [[nodiscard]] std::string Format(const std::string_view msg, Args&&... args) noexcept
+    {
+        try {
+            if(!msg.empty()) {
+                return fmt::vformat(msg, fmt::make_format_args(std::forward<Args>(args)...));
+            }
+        } catch(const fmt::format_error&) {
+            Log::Console<Log::Exception>(
+                "\n----------------------------------------\n"
+                "|| Error: format syntax invalid!\n"
+                "|| Format: {}"
+                "\n----------------------------------------", msg);
+        } catch(const std::bad_alloc&) {
+            Log::Console<Log::Exception>(
+                "\n----------------------------------------\n"
+                "|| Error: not enough memory for alloc\n"
+                "|| Format: {}"
+                "\n----------------------------------------", msg);
+        }
 
-		return std::string{};
-	}
+        return std::string{};
+    }
 }
 
 #endif // HELENA_UTIL_FORMAT_HPP
