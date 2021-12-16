a9e5d682ef0590cce4ee7cdca5e5822762fb1c84
NIKEA-SOFT <nsoft.sagaonline@gmail.com>
2021-12-16 16:15:56 +0300

    Refactoring

--- Helena/Util/Length.hpp
+++ Helena/Util/Length.hpp
@@ -1,41 +1,45 @@
 #ifndef HELENA_UTIL_LENGTH_HPP
 #define HELENA_UTIL_LENGTH_HPP
 
-#include <Helena/Debug/Assert.hpp>
+#include <cstdint>
+#include <cstddef>
+#include <limits>
 
 namespace Helena::Util
 {
-	enum class ELengthPolicy : std::uint8_t {
-		Truncate,
-		Fixed
-	};
+    enum class ELengthPolicy : std::uint8_t {
+        Truncate,
+        Fixed
+    };
 
-	template <typename Char = char>
-	[[nodiscard]] constexpr std::size_t Length(ELengthPolicy policy, const Char* data, std::size_t max_size = std::numeric_limits<std::size_t>::max()) noexcept 
-	{
-		if(!std::is_constant_evaluated()) {
-			HELENA_ASSERT(max_size, "Max size cannot be null");
-		}
+    template <typename Char = char>
+    [[nodiscard]] constexpr std::size_t Length(ELengthPolicy policy, const Char* data, std::size_t max_size = std::numeric_limits<std::size_t>::max()) noexcept
+    {
+        std::size_t offset = 0;
+        if(data) [[likely]]
+        {
+            while(true)
+            {
+                if(offset < max_size) [[likely]]
+                {
+                    if(data[offset] != '\0') [[likely]]
+                        ++offset;
+                    else [[unlikely]]
+                        break;
+                }
+                else [[unlikely]]
+                {
+                    switch(policy)
+                    {
+                        case ELengthPolicy::Truncate:   return offset;
+                        case ELengthPolicy::Fixed:      return 0uLL;
+                    }
+                }
+            }
+        }
 
-		std::size_t offset = 0;
-		if(data)
-		{ 
-			while(offset < max_size && data[offset] != '\0') { 
-				++offset; 
-			}
-
-			if(offset >= max_size) 
-			{
-				switch(policy) 
-				{
-					case ELengthPolicy::Truncate:	return offset - 1uLL;
-					case ELengthPolicy::Fixed:		return 0uLL;
-				}
-			}
-		}
-
-		return offset;
-	}
+        return offset;
+    }
 }
 
 #endif // HELENA_UTIL_LENGTH_HPP
