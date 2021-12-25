25f5645b93e5b2201443bb7bf4a0a8a303d91782
NIKEA-SOFT <nsoft.sagaonline@gmail.com>
2021-12-25 21:11:50 +0300

    Ternary constexpr if

---
+++ Helena/Util/ConstexprIf.hpp
@@ -0,0 +1,18 @@
+#ifndef HELENA_UTIL_CONSTEXPRIF_HPP
+#define HELENA_UTIL_CONSTEXPRIF_HPP
+
+#include <memory>
+
+namespace Helena::Util
+{
+    template <bool Condition, typename Success, typename Failure>
+    decltype(auto) ConstexprIf(Success&& lhs, Failure&& rhs) noexcept {
+        if constexpr (Condition) {
+            return std::forward<Success>(lhs);
+        } else {
+            return std::forward<Failure>(rhs);
+        }
+    }
+}
+
+#endif // HELENA_UTIL_CONSTEXPRIF_HPP
