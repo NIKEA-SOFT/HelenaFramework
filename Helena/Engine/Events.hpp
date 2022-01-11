1924c42996916b42e009b3a9a75777e410222cea
NIKEA-SOFT <nsoft.sagaonline@gmail.com>
2022-01-11 04:21:20 +0300

    Engine events header

---
+++ Helena/Engine/Events.hpp
@@ -0,0 +1,26 @@
+#ifndef HELENA_ENGINE_EVENTS_HPP
+#define HELENA_ENGINE_EVENTS_HPP
+
+namespace Helena::Events::Engine
+{
+    struct Init {};
+    struct Config {};
+    struct Execute {};
+
+    struct Tick {
+        float deltaTime;
+    };
+
+    struct Update {
+        float fixedTime;
+    };
+
+    struct Render {
+        float deltaTime;
+    };
+
+    struct Finalize {};
+    struct Shutdown {};
+}
+
+#endif // HELENA_ENGINE_EVENTS_HPP
