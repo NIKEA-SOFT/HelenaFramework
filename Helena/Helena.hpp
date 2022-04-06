bf8a5cb8a297d085e0c57be75454ad05673496f2
NIKEA-SOFT <nsoft.sagaonline@gmail.com>
2022-04-06 19:10:51 +0300

    support linux: gcc, clang and win: clang, msvc

--- Helena/Helena.hpp
+++ Helena/Helena.hpp
@@ -11,11 +11,10 @@
 #include <Helena/Platform/Processor.hpp>
 #include <Helena/Platform/Compiler.hpp>
 #include <Helena/Platform/Defines.hpp>
-
-// Debug
-#include <Helena/Debug/Assert.hpp>
+#include <Helena/Platform/Assert.hpp>
 
 // Engine
+#include <Helena/Engine/Events.hpp>
 #include <Helena/Engine/Engine.hpp>
 #include <Helena/Engine/Log.hpp>
 
@@ -26,6 +25,7 @@
 #include <Helena/Traits/CVRefPtr.hpp>
 #include <Helena/Traits/Detector.hpp>
 #include <Helena/Traits/FNV1a.hpp>
+#include <Helena/Traits/FunctionInfo.hpp>
 #include <Helena/Traits/IntegralConstant.hpp>
 #include <Helena/Traits/Map.hpp>
 #include <Helena/Traits/NameOf.hpp>
@@ -36,12 +36,15 @@
 
 // Types
 #include <Helena/Types/Any.hpp>
+#include <Helena/Types/BasicLoggersDef.hpp>
+#include <Helena/Types/BasicLogger.hpp>
 #include <Helena/Types/BenchmarkScoped.hpp>
 #include <Helena/Types/DateTime.hpp>
 #include <Helena/Types/Delegate.hpp>
 #include <Helena/Types/FixedBuffer.hpp>
 #include <Helena/Types/Format.hpp>
 #include <Helena/Types/Hash.hpp>
+#include <Helena/Types/LocationString.hpp>
 #include <Helena/Types/Monostate.hpp>
 #include <Helena/Types/Mutex.hpp>
 #include <Helena/Types/SourceLocation.hpp>
@@ -55,6 +58,7 @@
 
 // Util
 #include <Helena/Util/Cast.hpp>
+#include <Helena/Util/ConstexprIf.hpp>
 #include <Helena/Util/Format.hpp>
 #include <Helena/Util/Length.hpp>
 #include <Helena/Util/Sleep.hpp>
