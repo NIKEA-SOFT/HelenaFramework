6ac05b65f578e69ab0f9775b749e6341cfa2f43c
NIKEA-SOFT <nsoft.sagaonline@gmail.com>
2022-05-19 00:23:41 +0300

    Refactoring UniqueIndexer

--- Helena/Types/UniqueIndexer.hpp
+++ Helena/Types/UniqueIndexer.hpp
@@ -13,7 +13,7 @@ namespace Helena::Types
     class UniqueIndexer
     {
     public:
-        using key_type      = typename Traits::FNV1a<std::uint64_t>::value_type;
+        using hash_type     = Hash<std::uint64_t>;
         using index_type    = std::size_t;
         using storage_type  = std::vector<index_type>;
 
@@ -29,10 +29,11 @@ namespace Helena::Types
         [[nodiscard]] index_type Get() const {
             static_assert(std::is_same_v<T, Traits::RemoveCVRefPtr<T>>, "Type is const/ptr/ref");
             static const auto index = TypeIndexer<T>::GetIndex(m_Indexes);
+            HELENA_ASSERT(index < m_Indexes.size(), "UniqueIndexer with same UniqueKey should not be in multiple instances!");
             return index;
         }
 
-        [[nodiscard]] std::size_t GetSize() const noexcept {
+        [[nodiscard]] std::size_t Size() const noexcept {
             return m_Indexes.size();
         }
 
@@ -40,30 +41,20 @@ namespace Helena::Types
         template <typename T>
         struct TypeIndexer
         {
-            static constexpr auto m_Key = Types::Hash::template Get<T, std::uint64_t>();
+            [[nodiscard]] static auto GetIndex(const storage_type& storage)
+            {
+                if(const auto it = std::find(storage.cbegin(), storage.cend(), m_Key); it != storage.cend()) {
+                    return std::distance(storage.cbegin(), it) - 1uLL;
+                }
 
-            [[nodiscard]] static auto GetIndex(storage_type& storage) {
-                // Get a name of type T and generate a hash to use as a key for a hash map
-                const auto index = GetIndexByKey(storage, m_Key);
-                HELENA_ASSERT(index < storage.size(), "UniqueIndexer with same UniqueKey should not be in multiple instances!");
-                return index;
+                const_cast<storage_type&>(storage).emplace_back(m_Key);
+                return storage.size() - 1uLL;
             }
+
+            static constexpr auto m_Key = hash_type::template Get<T>();
         };
 
-        [[nodiscard]] static auto GetIndexByKey(storage_type& storage, const index_type key) {
-
-            for(std::size_t i = 0; i < storage.size(); ++i) 
-            {
-                if(storage[i] == key) {
-                    return i;
-                }
-            }
-            
-            storage.emplace_back(key);
-            return storage.size() - 1;
-        }
-
-        mutable storage_type m_Indexes;
+        storage_type m_Indexes;
     };
 
 }
