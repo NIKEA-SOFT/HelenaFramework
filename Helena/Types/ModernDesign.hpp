b179d5433047180ee297b0b14a18d52cc0392efc
NIKEA-SOFT <nsoft.sagaonline@gmail.com>
2022-08-02 11:45:36 +0300

    Change implementation for support across boundary

--- Helena/Types/ModernDesign.hpp
+++ Helena/Types/ModernDesign.hpp
@@ -1,7 +1,7 @@
 #ifndef HELENA_TYPES_MODERNDESIGN_HPP
 #define HELENA_TYPES_MODERNDESIGN_HPP
 
-#include <type_traits>
+#include <Helena/Engine/Engine.hpp>
 
 namespace Helena::Types
 {
@@ -10,19 +10,18 @@ namespace Helena::Types
     class ModernDesign
     {
     public:
-        ModernDesign() noexcept {
-            System = static_cast<T*>(this);
-        }
-        ~ModernDesign() noexcept {
-            System = nullptr;
-        }
+        ModernDesign() noexcept = default;
+        ~ModernDesign() noexcept = default;
         ModernDesign(const ModernDesign&) noexcept = delete;
         ModernDesign& operator=(const ModernDesign&) noexcept = delete;
         ModernDesign(ModernDesign&&) noexcept = delete;
         ModernDesign& operator=(ModernDesign&&) noexcept = delete;
 
     protected:
-        static inline T* System = nullptr;
+        [[nodiscard]] static T* CurrentSystem() {
+            const auto result = Helena::Engine::HasSystem<T>();
+            return result ? std::addressof(Helena::Engine::GetSystem<T>()) : nullptr;
+        }
     };
 }
 
