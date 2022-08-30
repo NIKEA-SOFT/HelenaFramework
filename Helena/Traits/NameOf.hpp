d6b92e296284b584934de3f3067aba0f8452f2c2
NIKEA-SOFT <nsoft.sagaonline@gmail.com>
2022-08-30 07:29:52 +0300

    Refactoring design

--- Helena/Traits/NameOf.hpp
+++ Helena/Traits/NameOf.hpp
@@ -10,12 +10,12 @@ namespace Helena::Traits
     template <typename T>
     class NameOf final {
         template <std::size_t... Indexes>
-        [[nodiscard]] static constexpr auto substring_as_array(std::string_view str, std::index_sequence<Indexes...>) noexcept {
+        [[nodiscard]] static constexpr auto Substring(std::string_view str, std::index_sequence<Indexes...>) noexcept {
             return std::array{str[Indexes]..., '\0'};
         }
 
         template <typename>
-        [[nodiscard]] static constexpr auto type_name_array() noexcept
+        [[nodiscard]] static constexpr auto ParseName() noexcept
         {
         #if defined(HELENA_COMPILER_CLANG)
             constexpr auto prefix   = std::string_view{"[T = "};
@@ -26,7 +26,7 @@ namespace Helena::Traits
             constexpr auto suffix   = std::string_view{"]"};
             constexpr auto function = std::string_view{__PRETTY_FUNCTION__};
         #elif defined(HELENA_COMPILER_MSVC)
-            constexpr auto prefix   = std::string_view{"type_name_array<"};
+            constexpr auto prefix   = std::string_view{"ParseName<"};
             constexpr auto suffix   = std::string_view{">(void)"};
             constexpr auto function = std::string_view{__FUNCSIG__};
         #else
@@ -35,30 +35,29 @@ namespace Helena::Traits
 
             constexpr auto start = function.find(prefix) + prefix.size();
             constexpr auto end = function.rfind(suffix);
-
             static_assert(start < end);
-
             constexpr auto name = function.substr(start, (end - start));
-            return substring_as_array(name, std::make_index_sequence<name.size()>{});
+            return Substring(name, std::make_index_sequence<name.size()>{});
         }
 
         template <typename Type>
-        struct type_name_holder {
-            static constexpr auto value = type_name_array<T>();
-        };
+        static constexpr auto NameStorage = ParseName<T>();
 
         template <typename Type>
-        [[nodiscard]] static constexpr auto type_name() {
-            constexpr auto& value = type_name_holder<Type>::value;
-            return std::string_view{value.data()};
+        [[nodiscard]] static constexpr auto Name() {
+            return std::string_view{NameStorage<Type>.data()};
         }
 
     public:
-        constexpr operator std::string_view() const noexcept {
-            return value;
+        static constexpr auto Value = Name<T>();
+
+        constexpr operator const char*() const noexcept {
+            return Value.data();
         }
 
-        static constexpr auto value = type_name<T>();
+        constexpr operator std::string_view() const noexcept {
+            return Value;
+        }
     };
 }
 
