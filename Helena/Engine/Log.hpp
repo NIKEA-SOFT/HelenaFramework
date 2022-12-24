4538b1e62792a536ef721609c6fadd6e48762399
NIKEA-SOFT <nsoft.sagaonline@gmail.com>
2022-12-24 22:44:27 +0300

    New message types are now available to everyone

--- Helena/Engine/Log.hpp
+++ Helena/Engine/Log.hpp
@@ -19,19 +19,25 @@ namespace Helena::Log
 #define HELENA_MSG(prefix, fmt, ...)        Helena::Log::Console<prefix>(fmt __VA_OPT__(,) __VA_ARGS__)
 
 #if defined(HELENA_DEBUG)
+    #define HELENA_MSG_ASSERT(fmt, ...)     HELENA_MSG(Helena::Log::Assert,     fmt __VA_OPT__(,) __VA_ARGS__)
     #define HELENA_MSG_DEBUG(fmt, ...)      HELENA_MSG(Helena::Log::Debug,      fmt __VA_OPT__(,) __VA_ARGS__)
     #define HELENA_MSG_INFO(fmt, ...)       HELENA_MSG(Helena::Log::Info,       fmt __VA_OPT__(,) __VA_ARGS__)
     #define HELENA_MSG_NOTICE(fmt, ...)     HELENA_MSG(Helena::Log::Notice,     fmt __VA_OPT__(,) __VA_ARGS__)
     #define HELENA_MSG_WARNING(fmt, ...)    HELENA_MSG(Helena::Log::Warning,    fmt __VA_OPT__(,) __VA_ARGS__)
     #define HELENA_MSG_ERROR(fmt, ...)      HELENA_MSG(Helena::Log::Error,      fmt __VA_OPT__(,) __VA_ARGS__)
     #define HELENA_MSG_FATAL(fmt, ...)      HELENA_MSG(Helena::Log::Fatal,      fmt __VA_OPT__(,) __VA_ARGS__)
+    #define HELENA_MSG_EXCEPTION(fmt, ...)  HELENA_MSG(Helena::Log::Exception,  fmt __VA_OPT__(,) __VA_ARGS__)
+    #define HELENA_MSG_MEMORY(fmt, ...)     HELENA_MSG(Helena::Log::Memory,     fmt __VA_OPT__(,) __VA_ARGS__)
 #else
+    #define HELENA_MSG_ASSERT(fmt, ...)
     #define HELENA_MSG_DEBUG(fmt, ...)
     #define HELENA_MSG_INFO(fmt, ...)
     #define HELENA_MSG_NOTICE(fmt, ...)     HELENA_MSG(Helena::Log::Notice,     fmt __VA_OPT__(,) __VA_ARGS__)
     #define HELENA_MSG_WARNING(fmt, ...)    HELENA_MSG(Helena::Log::Warning,    fmt __VA_OPT__(,) __VA_ARGS__)
     #define HELENA_MSG_ERROR(fmt, ...)      HELENA_MSG(Helena::Log::Error,      fmt __VA_OPT__(,) __VA_ARGS__)
     #define HELENA_MSG_FATAL(fmt, ...)      HELENA_MSG(Helena::Log::Fatal,      fmt __VA_OPT__(,) __VA_ARGS__)
+    #define HELENA_MSG_EXCEPTION(fmt, ...)  HELENA_MSG(Helena::Log::Exception,  fmt __VA_OPT__(,) __VA_ARGS__)
+    #define HELENA_MSG_MEMORY(fmt, ...)
 #endif
 
 #endif // HELENA_ENGINE_LOG_HPP
