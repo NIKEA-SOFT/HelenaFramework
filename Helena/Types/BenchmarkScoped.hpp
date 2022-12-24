95346d007ae4d5c1ad05825ce6d37379feda818a
NIKEA-SOFT <nsoft.sagaonline@gmail.com>
2022-12-24 22:55:40 +0300

    Benchmark logger declaration moved to Log namespace

--- Helena/Types/BenchmarkScoped.hpp
+++ Helena/Types/BenchmarkScoped.hpp
@@ -12,27 +12,18 @@ namespace Helena::Types
     {
         using Timer = std::chrono::steady_clock;
 
-        struct Benchmark {
-            [[nodiscard]] static constexpr auto GetPrefix() noexcept {
-                return Log::CreatePrefix("[Benchmark:");
-            }
-
-            [[nodiscard]] static constexpr auto GetStyle() noexcept {
-                return Log::CreateStyle(Log::Color::BrightMagenta);
-            }
-        };
-
     public:
         BenchmarkScoped(const SourceLocation& location = SourceLocation::Create()) : m_Location{location}, m_Time{Timer::now()} {}
         ~BenchmarkScoped() {
             const std::chrono::duration<float> timeleft = Timer::now() - m_Time;
-            const auto formater = Log::Formater<Benchmark>{"{}] Timeleft: {:.6f} sec", m_Location};
-            Log::Console<Benchmark>(formater, m_Location.GetFunction(), timeleft.count());
+            const auto formater = Log::Formater<Log::Benchmark>{"{}] Timeleft: {:.6f} sec", m_Location};
+            Log::Console<Log::Benchmark>(formater, m_Location.GetFunction(), timeleft.count());
         }
         BenchmarkScoped(const BenchmarkScoped&) = delete;
         BenchmarkScoped(BenchmarkScoped&&) noexcept = delete;
         BenchmarkScoped& operator=(const BenchmarkScoped&) = delete;
         BenchmarkScoped& operator=(BenchmarkScoped&&) noexcept = delete;
+
     private:
         SourceLocation m_Location;
         Timer::time_point m_Time;
