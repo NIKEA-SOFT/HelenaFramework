adf76a24edc4ca697e5461b892c2d88d2303f4f2
NIKEA-SOFT <nsoft.sagaonline@gmail.com>
2023-02-10 16:40:21 +0300

    Added overloads with Engine::NoSignal parameter

--- Systems/ResourceManager.hpp
+++ Systems/ResourceManager.hpp
@@ -1,6 +1,7 @@
 #ifndef HELENA_SYSTEMS_RESOURCEMANAGER_HPP
 #define HELENA_SYSTEMS_RESOURCEMANAGER_HPP
 
+#include <Helena/Types/ModernDesign.hpp>
 #include <Helena/Types/VectorAny.hpp>
 
 namespace Helena::Events::ResourceManager
@@ -36,17 +37,33 @@ namespace Helena::Systems
         ResourceManager& operator=(const ResourceManager&) = delete;
         ResourceManager& operator=(ResourceManager&&) noexcept = delete;
 
+        template <typename Resource>
+        static void Create(decltype(Engine::NoSignal), Resource&& instance) {
+            CurrentSystem().m_Storage.template Create<Traits::RemoveCVRP<Resource>>(std::forward<Resource>(instance));
+        }
+
         template <typename Resource, typename... Args>
         requires Traits::SameAs<Resource, Traits::RemoveCVRP<Resource>>
-        static void Create(Args&&... args) {
-            Engine::SignalEvent<Events::ResourceManager::PreCreate<Resource>>();
+        static void Create(decltype(Engine::NoSignal), Args&&... args) {
             CurrentSystem().m_Storage.template Create<Resource>(std::forward<Args>(args)...);
-            Engine::SignalEvent<Events::ResourceManager::PostCreate<Resource>>(CurrentSystem().m_Storage.template Get<Resource>());
         }
 
         template <typename Resource>
         static void Create(Resource&& instance) {
-            Create<Traits::RemoveCVRP<Resource>>(std::forward<Resource>(instance));
+            auto& currentSystem = CurrentSystem();
+            Engine::SignalEvent<Events::ResourceManager::PreCreate<Traits::RemoveCVRP<Resource>>>();
+            currentSystem.m_Storage.template Create<Traits::RemoveCVRP<Resource>>(std::forward<Resource>(instance));
+            Engine::SignalEvent<Events::ResourceManager::PostCreate<Traits::RemoveCVRP<Resource>>>(
+                currentSystem.m_Storage.template Get<Traits::RemoveCVRP<Resource>>());
+        }
+
+        template <typename Resource, typename... Args>
+        requires Traits::SameAs<Resource, Traits::RemoveCVRP<Resource>>
+        static void Create(Args&&... args) {
+            auto& currentSystem = CurrentSystem();
+            Engine::SignalEvent<Events::ResourceManager::PreCreate<Resource>>();
+            currentSystem.m_Storage.template Create<Resource>(std::forward<Args>(args)...);
+            Engine::SignalEvent<Events::ResourceManager::PostCreate<Resource>>(currentSystem.m_Storage.template Get<Resource>());
         }
 
         template <typename... Resources>
@@ -69,9 +86,16 @@ namespace Helena::Systems
 
         template <typename... Resources>
         requires (Traits::SameAs<Resources, Traits::RemoveCVRP<Resources>> && ...)
-        static void Remove() {
-            (Engine::SignalEvent<Events::ResourceManager::PreRemove<Resources>>(CurrentSystem().m_Storage.template Get<Resources>()), ...);
+        static void Remove(decltype(Engine::NoSignal)) {
             CurrentSystem().m_Storage.template Remove<Resources...>();
+        }
+
+        template <typename... Resources>
+        requires (Traits::SameAs<Resources, Traits::RemoveCVRP<Resources>> && ...)
+        static void Remove() {
+            auto& currentSystem = CurrentSystem();
+            (Engine::SignalEvent<Events::ResourceManager::PreRemove<Resources>>(currentSystem.m_Storage.template Get<Resources>()), ...);
+            currentSystem.m_Storage.template Remove<Resources...>();
             (Engine::SignalEvent<Events::ResourceManager::PostRemove<Resources>>(), ...);
         }
 
