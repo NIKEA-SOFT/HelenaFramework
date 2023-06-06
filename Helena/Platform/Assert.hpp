66a28f6e6707aa0dd39a7b89fb08c4108f3ea1e9
NIKEA-SOFT <nsoft.sagaonline@gmail.com>
2023-06-06 14:54:44 +0300

    Added macros: HELENA_ASSERT_RUNTIME for runtime assert NOTE: For Window application need specialization CustomPrint<Helena::Log::Assert> for write assert messages in GUI or file

--- Helena/Platform/Assert.hpp
+++ Helena/Platform/Assert.hpp
@@ -2,29 +2,28 @@
 #define HELENA_PLATFORM_ASSERT_HPP
 
 #include <Helena/Types/BasicLogger.hpp>
+#include <tuple>
+
+#define HELENA_ASSERT_RUNTIME(cond, ...)                                        \
+if(!std::is_constant_evaluated()) {                                             \
+    if(!(cond)) {                                                               \
+        Helena::Log::Message<Helena::Log::Assert>("Condition: " #cond);         \
+                                                                                \
+        using tuple = decltype(std::forward_as_tuple(__VA_ARGS__));             \
+        if constexpr(std::tuple_size_v<tuple> > 0) {                            \
+            Helena::Log::Message<Helena::Log::Assert>("Message: " __VA_ARGS__); \
+        }                                                                       \
+                                                                                \
+        if(HELENA_DEBUGGING()) {                                                \
+            HELENA_BREAKPOINT();                                                \
+        }                                                                       \
+                                                                                \
+        std::exit(EXIT_FAILURE);                                                \
+    }                                                                           \
+}
 
 #ifdef HELENA_DEBUG
-    #include <tuple>
-
-    #define HELENA_ASSERT(cond, ...)                                                    \
-    if(!std::is_constant_evaluated()) {                                                 \
-        do {                                                                            \
-            if(!(cond)) {                                                               \
-                Helena::Log::Message<Helena::Log::Assert>("Condition: " #cond);         \
-                                                                                        \
-                using tuple = decltype(std::forward_as_tuple(__VA_ARGS__));             \
-                if constexpr(std::tuple_size_v<tuple> > 0) {                            \
-                    Helena::Log::Message<Helena::Log::Assert>("Message: " __VA_ARGS__); \
-                }                                                                       \
-                                                                                        \
-                if(HELENA_DEBUGGING()) {                                                \
-                    HELENA_BREAKPOINT();                                                \
-                }                                                                       \
-                                                                                        \
-                std::exit(EXIT_FAILURE);                                                \
-            }                                                                           \
-        } while(false);                                                                 \
-    }
+    #define HELENA_ASSERT(cond, ...)    HELENA_ASSERT_RUNTIME(cond, __VA_ARGS__)
 #else
     #define HELENA_ASSERT(cond, ...)
 #endif
