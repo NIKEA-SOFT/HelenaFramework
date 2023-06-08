4885709dcb07307c52b83ac93798326800e431b3
NIKEA-SOFT <nsoft.sagaonline@gmail.com>
2023-06-08 06:01:33 +0300

    Added lessons

---
+++ Examples/Lesson1_Init/main.cpp
@@ -0,0 +1,152 @@
+﻿#include <Helena/Engine/Engine.hpp>
+#include <iostream>
+
+// Lesson 1: Initialization, Heartbeat and Shutdown
+
+int main(int argc, char** argv)
+{
+    // Framework initialization
+    // This leads to the creation of a context class.
+    // Context is a class that stores all the systems, signals, timers and many more.
+    // NOTE: There is no method: Finalize, this is not necessary,
+    // it will complete itself correctly when the application closes.
+    Helena::Engine::Initialize();
+
+    // Framework (non blocking) loop
+    // Heartbeat returns false on shutdown or failure
+    // This method must be called constantly in a loop to keep the signals working.
+    while(Helena::Engine::Heartbeat())
+    {
+        // other your operations
+        // You may be wondering "Why would I insert operations here when I can do it with signals of frameworks?"
+        // If you asked this question, then it means that you already know about signals and therefore
+        // I will explain it to you right here, and for those who are not yet familiar, about signals in the following lessons.
+        // Answer:
+        // That's right, you can insert your operations in callbacks of signals or inside Systems, we will also talk about them in other lessons.
+        // In fact, everything is much simpler, the framework was developed so that it could be integrated into projects at any stage, that's all.
+        std::cout << "Hello from loop" << std::endl;
+
+        // or shutdown framework
+        // Note: the Shutdown method can take arguments,
+        // but the call by default means the framework exits without any errors
+        // This method is thread safe.
+        Helena::Engine::Shutdown();
+
+        // here example how shutdown framework with error reason.
+        // Note: if you use an overload with arguments, then the framework
+        // will notify the reason for shutting down in the console.
+        // WARNING: If your application is not a console application,
+        // then the messages will not be printed, you must redirect the
+        // output of the logs, but I will talk about this in the next lessons
+        Helena::Engine::Shutdown("Test error");
+        // or with formatting
+        Helena::Engine::Shutdown("Shutdown reason: {}", "test error");
+
+        // NOTE:
+        // and yes... we can call shutdown multiple time
+        // this has no effect if the framework has already been terminated
+        // but you must remember that if you called Shutdown with no arguments,
+        // then the next call to Shutdown, even with a reason, will not set this message (reason),
+        // since the state has already passed to Shutdown
+
+
+
+        // When we call Shutdown with a reason, we can get that reason at any time (after shutdown to).
+        // If Shutdown was called with no arguments, then this method returns an empty string.
+        const auto& reason = Helena::Engine::ShutdownReason();
+
+
+        // NOTE:
+        // After calling Shutdown, the framework will enter the Finalize state,
+        // but it will only know about completion when the next call to Heartbeat occurs.
+        // You must understand that Shutdown does not exit immediately from process,
+        // it only change the state to the Shutdown state, the new state will be processed on the next call to Heartbeat
+
+        // However, we can check the new state after Shutdown immediately
+        // This method is thread safe
+        // About method Running - this function is the same as calling Helena::Engine::GetState() == Helena::Engine::EState::Init
+        if(Helena::Engine::Running()) {
+            std::cout << "The framework is still running!" << std::endl;
+        } else {
+            // This message will be displayed because the call to Shutdown switches the state immediately
+            // In order not to confuse you, let me explain:
+            // the Shutdown call changes the State inside the kernel, however,
+            // the signals (we will talk about in future lessons) will only be thrown after the Heartbeat call
+            // The kernel (framework) uses a state machine, so a state change has consequences after the Heartbeat is called again.
+            std::cout << "The framework has entered shutdown state! #1" << std::endl;
+        }
+
+        // Other method for get state
+        // This method is thread safe.
+        switch(Helena::Engine::GetState())
+        {
+            // Undefined state:
+            // This state means that the framework has not yet had time to initialize.
+            // The main thing here is not to get confused, when you call Initialize,
+            // you only set the Undefined state, this state switches to the Init state only
+            // after the first call to Heartbeat, in fact, this state is used to initialize
+            // some fields inside the kernel, after which the state will change to Init.
+            // In short, the Undefined state is only possible in two cases:
+            // 1. You called Helena::Engine::Initialize, but the first call to Helena::Engine::Heartbeat has not yet occurred.
+            // 2. You called Helena::Engine::Shutdown and after Helena::Engine::Heartbeat is called, the state will change
+            // from Shutdown to Undefined again.
+            //
+            // This approach allows you to restart the loop inside the framework without shutting down the application even
+            // if you get an exception (some exceptions that were missed can be caught by the core of the
+            // framework to complete the correct operation of applications including signals)
+            // Tip: Think of the Undefined state as a car starter, the key is in the lock.
+            // This is the best analogy, because in this state you are either going on a trip, or you have completed it :)
+            case Helena::Engine::EState::Undefined: {
+                std::cout << "The framework is initialized, but state is Undefined" << std::endl;
+            } break;
+
+
+            // Init state:
+            // We get this state after the Undefined state.
+            // In this state, calls to the Heartbeat method process the main loop: timers, fixed steps, signals
+            case Helena::Engine::EState::Init: {
+                std::cout << "The framework is running!" << std::endl;
+            } break;
+
+            // Shutdown state:
+            // This state is set when Helena::Engine::Shutdown is called, or if your application crashes,
+            // such as an exception that was not caught, in which case the framework will set the
+            // Shutdown state on its own to try to properly shutdown your application, so you don't lose data.
+            case Helena::Engine::EState::Shutdown: {
+                std::cout << "The framework has entered shutdown state! #2" << std::endl;
+            } break;
+        }
+    }
+
+    // Important Notes:
+    // 1) Don't call Helena::Engine::Initialize again, even after calling Helena::Engine::Shutdown,
+    // shutdown is just one of the states, not Finalize, the framework will take care of the
+    // shutdown when the application closes.
+    // 2) You can restart the loop if you want to restart all applications without having to end the
+    // process and start it again, however, you must remember that the systems and all signals will be cleared,
+    // you will have to register them again. Therefore, it's like restarting the process.
+
+
+    // Now we know how to initialize the framework.
+    // Let's repeat, but without long comments.
+
+    // HasContext used for check the initialization of the framework.
+    // About context in Lesson 2
+    if(!Helena::Engine::HasContext()) {
+        Helena::Engine::Initialize();
+    }
+
+    std::size_t сounter{};
+    while(Helena::Engine::Heartbeat())
+    {
+        // Shutdown after 10 iterations
+        if(++сounter == 10) {
+            // You will see this message in the console after next call Heartbeat, the reason of shutdown.
+            Helena::Engine::Shutdown("Shutdown after {} iterations!", сounter);
+        }
+
+        std::cout << "Counter = " << сounter << std::endl;
+    }
+
+    return 0;
+}
---
+++ Examples/Lesson2_Context/main.cpp
@@ -0,0 +1,134 @@
+﻿#include <Helena/Engine/Engine.hpp>
+#include <iostream>
+
+// Lesson 2: Context and how to use your own context in Helena
+// A context is a class within the Engine that stores complex structures within itself
+// that provide access to your systems and signals in O(1) using type indexing.
+// The context also provides support for type indexing across boundaries (dll, so plugins).
+// When you use Helena::Engine::Initialize it will initialize the default context which has no public fields or methods.
+// This decision is due to the fact that I did not want to complicate the API of the framework,
+// now all operations are performed only through the Engine class, which contains completely static methods,
+// which frees the developer from responsibility for storing the context, engine and other data.
+// In Lesson 1 there was an example of how we initialize the default context, of course the method being called doesn't
+// say anything about it, but it is.
+//
+// In the current lesson, I will show you how to create your own context and what it can be useful for.
+
+
+// Let's declare a class that defines our own context
+class MyContext : public Helena::Engine::Context
+{
+public:
+    // *Let's declare the constructors and log them so you can trace the sequence of calls
+    MyContext() {
+        std::cout << "MyContext ctor called" << std::endl;
+    }
+
+    ~MyContext() {
+        std::cout << "MyContext dtor called" << std::endl;
+    }
+
+public:
+    // We can override our own entry point (main)
+    // This is not required, but you can use it.
+    // This Main does not return the result of the operation.
+    // To report an error use Helena::Engine::Shutdown
+    void Main() override
+    {
+        // *Let's leave this log to see the sequence of calls
+        std::cout << "Main called" << std::endl;
+
+        // A small example so you can play with it
+        bool hasError = true; // change to "true" after checking the result
+        if(hasError)
+        {
+            // Change to "true" for show example with reason message for shutdown
+            bool hasReason = false;
+
+            if(!hasReason) {
+                // Example #1 | Shutdown without message, this means that no errors occurred.
+                Helena::Engine::Shutdown();
+            } else {
+                // Example #2 | Shutdown with message (reason)
+                Helena::Engine::Shutdown("Test shutdown with my test error!");
+            }
+
+            return;
+        }
+
+        // other operations...
+        m_Counter = 10;
+    }
+
+public:
+    std::size_t m_Counter{};
+};
+
+int main(int argc, char** argv)
+{
+    // Initializing Your Own Context
+    // This will create MyContext and then call Main if it has been overridden.
+    // You can also pass arguments to the constructor.
+    // The call sequence is as follows:
+    // Initialize -> Create Context object -> Call Main (if overrided)
+    Helena::Engine::Initialize<MyContext>(/* args... */);
+
+    // You may notice that the Initialize method returns void and
+    // wonder "How do you know if there was a failure during initialization?"
+    // It's really simple, use Helena::Engine::GetState()
+    // In Lesson 1, we already got acquainted with this function.
+    // Let me remind you that the method is thread-safe.
+    if(Helena::Engine::GetState() == Helena::Engine::EState::Shutdown) {
+        // We can also get the reason for the failure.
+        // NOTE: I'm not sure if there is a need for this,
+        // since it is possible to override the output for any type of log message.
+        // I will talk about this in the future...
+        const auto& reason = Helena::Engine::ShutdownReason();
+        if(reason.empty()) {
+            std::cout << "Context initialize failed, shutdown without error (reason)" << std::endl;
+        } else {
+            std::cout << reason << std::endl;
+        }
+
+        // Important note:
+        // In fact, Heartbeat will process the Shutdown itself and you won't get into the
+        // body of the loop anyway, so you don't need to return and check the state on Shutdown,
+        // only use this if your code needs it.
+        // In addition, Heartbeat will show you the reason for the shutdown in console
+    } else {
+        std::cout << "Context initialized successfully!" << std::endl;
+    }
+
+    std::cout << "Before call Heartbeat" << std::endl;
+
+    // I didn't mention it in the Lesson 1, but the Heartbeat method is also capable of taking arguments.
+    // Please read about arguments in method declaration, where it is described in more detail.
+    const auto sleepMS = 1; // 1 ms
+    const auto accumulator = 5; // High load update regulator (delta accumulator)
+    while(Helena::Engine::Heartbeat(sleepMS, accumulator))
+    {
+        // Get a reference to your context
+        // The method is not thread-safe, but it's actually safe to get since
+        // you initialize it once and then it always exists until the application exits.
+        auto& ctx = Helena::Engine::GetContext<MyContext>();
+
+        // Shutdown the framework when the counter == 0
+        if(ctx.m_Counter == 0) {
+            // We do not specify the reason, which means that no errors occurred.
+            Helena::Engine::Shutdown();
+
+            // There is no need to write "break", this is wrong!
+            // The framework itself must break the loop, as it has signals to notify everyone about the shutdown.
+            // But you can use "continue" if you don't want to execute other logic in this loop due to the call to Shutdown.
+            continue;
+        }
+
+        // Yes, we can continue to use the context after Shutdown,
+        // Remember: the context lives the entire life cycle of the program after calling Helena::Engine::Initialize
+        std::cout << "Counter: " << ctx.m_Counter-- << std::endl;
+    }
+
+    // - Now we know how to initialize our own context in the framework.
+
+    return 0;
+}
