f6d30fc7aea4ee9e10bd5959f4761652718a9bde
NIKEA-SOFT <nsoft.sagaonline@gmail.com>
2023-06-13 19:03:05 +0300

    Test action

--- Helena/Util/Function.hpp
+++ Helena/Util/Function.hpp
@@ -10,7 +10,7 @@ namespace Helena::Util
     {
         template <typename Fn, typename... Args>
         [[nodiscard]] static constexpr auto BindFront(Fn&& fn, Args&&... args) noexcept {
-            return [fn = std::forward<Fn>(fn), ...frontArgs = std::forward<Args>(args)](auto&&... args) constexpr -> decltype(auto)
+            return [fn = std::forward<Fn>(fn), ...frontArgs = std::forward<Args>(args)](auto&&... args) -> decltype(auto)
             requires(std::invocable<Fn, Args..., decltype(args)...>) {
                 return std::invoke(fn, frontArgs..., std::forward<decltype(args)>(args)...);
             };
@@ -18,7 +18,7 @@ namespace Helena::Util
 
         template <typename Fn, typename... Args>
         [[nodiscard]] static constexpr auto BindBack(Fn&& fn, Args&&... args) noexcept {
-            return [fn = std::forward<Fn>(fn), ...backArgs = std::forward<Args>(args)](auto&&... args) constexpr -> decltype(auto)
+            return [fn = std::forward<Fn>(fn), ...backArgs = std::forward<Args>(args)](auto&&... args) -> decltype(auto)
             requires(std::invocable<Fn, decltype(args)..., Args...>) {
                 return std::invoke(fn, std::forward<decltype(args)>(args)..., backArgs...);
             };
