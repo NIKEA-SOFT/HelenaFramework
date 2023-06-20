81496048b0d9595a88294eb410309799d6fa9347
NIKEA-SOFT <nsoft.sagaonline@gmail.com>
2023-06-21 02:18:32 +0300

    Added alias to get class type

--- Helena/Traits/Function.hpp
+++ Helena/Traits/Function.hpp
@@ -11,16 +11,19 @@ namespace Helena::Traits
 
     template <typename Ret, typename T, typename... Args>
     struct Function<Ret(T::*)(Args...) const> : Identity<Ret(T::*)(Args...) const>, Arguments<Args...> {
+        using Class = T;
         using Return = Ret;
     };
 
     template <typename Ret, typename T, typename... Args>
     struct Function<Ret(T::*)(Args...)> : Identity<Ret(T::*)(Args...)>, Arguments<Args...> {
+        using Class = T;
         using Return = Ret;
     };
 
     template <typename Ret, typename... Args>
     struct Function<Ret(*)(Args...)> : Identity<Ret(*)(Args...)>, Arguments<Args...> {
+        using Class = void;
         using Return = Ret;
     };
 }
