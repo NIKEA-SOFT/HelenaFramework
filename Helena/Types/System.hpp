2e3feacdd4387c0b7522e604e95155b9d4518200
NIKEA-SOFT <nsoft.sagaonline@gmail.com>
2023-07-23 19:04:27 +0300

    Abstraction with static methods doesn't need constructors

--- Helena/Types/System.hpp
+++ Helena/Types/System.hpp
@@ -9,13 +9,6 @@ namespace Helena::Types
     requires std::is_class_v<T>
     struct System
     {
-        System() noexcept = default;
-        ~System() noexcept = default;
-        System(const System&) noexcept = delete;
-        System& operator=(const System&) noexcept = delete;
-        System(System&&) noexcept = delete;
-        System& operator=(System&&) noexcept = delete;
-
         [[nodiscard]] static T& CurrentSystem() {
             HELENA_ASSERT(Engine::HasSystem<T>(), "System: {} not registered!", Traits::NameOf<T>);
             return Engine::GetSystem<T>();
