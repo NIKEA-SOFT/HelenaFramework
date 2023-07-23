5ae5af10408396adde6f4df6a81d9554951709ca
NIKEA-SOFT <nsoft.sagaonline@gmail.com>
2023-07-23 19:10:41 +0300

    Refactoring: UniquePointer is better

--- Helena/Types/VectorKVAny.hpp
+++ Helena/Types/VectorKVAny.hpp
@@ -1,13 +1,12 @@
 #ifndef HELENA_TYPES_VECTORKVANY_HPP
 #define HELENA_TYPES_VECTORKVANY_HPP
 
-#include <Helena/Traits/Add.hpp>
 #include <Helena/Traits/Arguments.hpp>
 #include <Helena/Traits/NameOf.hpp>
-#include <Helena/Traits/Remove.hpp>
-#include <Helena/Traits/SameAs.hpp>
-#include <Helena/Types/UndefinedContainer.hpp>
 #include <Helena/Types/UniqueIndexer.hpp>
+
+#include <vector>
+#include <memory>
 
 namespace Helena::Types
 {
@@ -15,101 +14,98 @@ namespace Helena::Types
     class VectorKVAny final
     {
     public:
+        using UniquePointer = std::unique_ptr<void, void (*)(const void*)>;
+
+        template <typename T>
+        static constexpr bool AllowedParam = std::conjunction_v<
+            std::is_same<std::remove_cvref_t<std::remove_pointer_t<T>>, T>,
+            std::negation<std::is_same<T, void>>,
+            std::negation<std::is_function<std::remove_pointer_t<T>>>,
+            std::negation<std::is_member_function_pointer<T>>>;
+
+        template <typename Key, typename T, typename... Args>
+        static constexpr bool RequiredParams = std::conjunction_v<
+            std::bool_constant<AllowedParam<Key>>,
+            std::bool_constant<AllowedParam<T>>,
+            std::conditional_t<Traits::Arguments<Args...>::Orphan,
+                std::is_default_constructible<std::decay_t<T>>,
+                std::is_constructible<std::decay_t<T>, Args...>>>;
+
+    public:
         VectorKVAny() : m_TypeIndexer{}, m_Storage{} {}
         ~VectorKVAny() = default;
         VectorKVAny(const VectorKVAny&) = delete;
-        VectorKVAny(VectorKVAny&&) noexcept = delete;
+        VectorKVAny(VectorKVAny&&) noexcept = default;
         VectorKVAny& operator=(const VectorKVAny&) = delete;
-        VectorKVAny& operator=(VectorKVAny&&) noexcept = delete;
+        VectorKVAny& operator=(VectorKVAny&&) noexcept = default;
 
-        template <typename K, typename T, typename... Args>
-        requires (Traits::SameAs<K, Traits::RemoveCVRP<K>> && Traits::SameAs<T, Traits::RemoveCVRP<T>>)
+        template <typename Key, typename T, typename... Args>
+        requires RequiredParams<Key, T, Args...>
         void Create(Args&&... args)
         {
-            const auto index = m_TypeIndexer.template Get<K>();
+            const auto index = m_TypeIndexer.template Get<Key>();
             if(index >= m_Storage.size()) {
-                m_Storage.resize(index + 1);
+                m_Storage.emplace_back(nullptr, nullptr);
             }
 
             HELENA_ASSERT(!m_Storage[index], "Type: {} already exist!", Traits::NameOf<T>);
-            m_Storage[index].template Create<T>(std::forward<Args>(args)...);
+            m_Storage[index] = UniquePtr(new T(std::forward<Args>(args)...), +[](const void* ptr) {
+                delete static_cast<const T*>(ptr);
+            });
         }
 
-        template <typename... K>
-        requires (!Traits::Arguments<K...>::Orphan && (Traits::SameAs<K, Traits::RemoveCVRP<K>> && ...))
+        template <typename... Key>
+        requires (!Traits::Arguments<Key...>::Orphan && (AllowedParam<Key> && ...))
         [[nodiscard]] bool Has() const {
-            if constexpr(Traits::Arguments<K...>::Single) {
-                const auto index = m_TypeIndexer.template Get<K...>();
+            if constexpr(Traits::Arguments<Key...>::Single) {
+                const auto index = m_TypeIndexer.template Get<Key...>();
                 return index < m_Storage.size() && m_Storage[index];
-            } else return (Has<K>() && ...);
+            } else return (Has<Key>() && ...);
         }
 
-        template <typename... K>
-        requires (Traits::Arguments<K...>::Size > 1 && (Traits::SameAs<K, Traits::RemoveCVRP<K>> && ...))
+        template <typename... Key>
+        requires (Traits::Arguments<Key...>::Size > 1 && (AllowedParam<Key> && ...))
         [[nodiscard]] bool Any() const {
-            return (Has<K>() || ...);
+            return (Has<Key>() || ...);
         }
 
-        template <typename K, typename T>
-        requires (Traits::SameAs<K, Traits::RemoveCVRP<K>> && Traits::SameAs<T, Traits::RemoveCVRP<T>>)
-        [[nodiscard]] bool Contain() const {
-            const auto index = m_TypeIndexer.template Get<K>();
-            return index < m_Storage.size() && m_Storage[index].template Equal<T>();
-        }
-
-        template <typename K, typename T>
-        requires (Traits::SameAs<K, Traits::RemoveCVRP<K>> && Traits::SameAs<T, Traits::RemoveCVRP<T>>)
+        template <typename Key, typename T>
+        requires (AllowedParam<Key> && AllowedParam<T>)
         [[nodiscard]] decltype(auto) Get() {
-            const auto index = m_TypeIndexer.template Get<K>();
+            const auto index = m_TypeIndexer.template Get<Key>();
             HELENA_ASSERT(index < m_Storage.size() && m_Storage[index], "Type: {} not exist!", Traits::NameOf<T>);
-            HELENA_ASSERT(m_Storage[index].template Equal<T>(), "Type: {} type mismatch!", Traits::NameOf<T>);
-            return m_Storage[index].template Ref<T>();
+            return *static_cast<T*>(m_Storage[index].get());
         }
 
-        template <typename K, typename T>
-        requires (Traits::SameAs<K, Traits::RemoveCVRP<K>> && Traits::SameAs<T, Traits::RemoveCVRP<T>>)
+        template <typename Key, typename T>
+        requires (AllowedParam<Key> && AllowedParam<T>)
         [[nodiscard]] decltype(auto) Get() const {
-            const auto index = m_TypeIndexer.template Get<K>();
+            const auto index = m_TypeIndexer.template Get<Key>();
             HELENA_ASSERT(index < m_Storage.size() && m_Storage[index], "Type: {} not exist!", Traits::NameOf<T>);
-            HELENA_ASSERT(m_Storage[index].template Equal<T>(), "Type: {} type mismatch!", Traits::NameOf<T>);
-            return m_Storage[index].template Ref<T>();
+            return *static_cast<const T*>(m_Storage[index].get());
         }
 
-        template <typename K, typename T>
-        requires (Traits::SameAs<K, Traits::RemoveCVRP<K>> && Traits::SameAs<T, Traits::RemoveCVRP<T>>)
-        [[nodiscard]] decltype(auto) Ptr() {
-            const auto index = m_TypeIndexer.template Get<K>();
-            return index < m_Storage.size() ? m_Storage[index].template Ptr<T>() : nullptr;
-        }
-
-        template <typename K, typename T>
-        requires (Traits::SameAs<K, Traits::RemoveCVRP<K>> && Traits::SameAs<T, Traits::RemoveCVRP<T>>)
-        [[nodiscard]] decltype(auto) Ptr() const {
-            const auto index = m_TypeIndexer.template Get<K>();
-            return index < m_Storage.size() ? m_Storage[index].template Ptr<T>() : nullptr;
-        }
-
-        template <typename... K>
-        requires (!Traits::Arguments<K...>::Orphan && (Traits::SameAs<K, Traits::RemoveCVRP<K>> && ...))
+        template <typename... Key>
+        requires (!Traits::Arguments<Key...>::Orphan && (AllowedParam<Key> && ...))
         void Remove()
         {
-            if constexpr(Traits::Arguments<K...>::Single) {
-                const auto index = m_TypeIndexer.template Get<K...>();
-                HELENA_ASSERT(index < m_Storage.size() && m_Storage[index], "Key: {} not exist!", Traits::NameOf<K...>);
-                m_Storage[index].Reset();
-            } else (Remove<K>(), ...);
+            if constexpr(Traits::Arguments<Key...>::Single) {
+                const auto index = m_TypeIndexer.template Get<Key...>();
+                HELENA_ASSERT(index < m_Storage.size() && m_Storage[index], "Key: {} not exist!", Traits::NameOf<Key...>);
+                m_Storage[index].reset();
+            } else (Remove<Key>(), ...);
         }
 
         void Clear() noexcept
         {
             for(auto& value : m_Storage) {
-                value.Reset();
+                value.reset();
             }
         }
 
     private:
         UniqueIndexer<UniqueKey> m_TypeIndexer;
-        std::vector<UndefinedContainer> m_Storage;
+        std::vector<UniquePointer> m_Storage;
     };
 }
 
