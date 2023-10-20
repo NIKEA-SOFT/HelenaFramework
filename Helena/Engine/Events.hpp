02b05e5a9e9668c550eea26a5f29ca2dcbff7646
NIKEA-SOFT <nsoft.sagaonline@gmail.com>
2023-10-20 06:30:54 +0300

    New events for Systems and Components

--- Helena/Engine/Events.hpp
+++ Helena/Engine/Events.hpp
@@ -7,16 +7,13 @@ namespace Helena::Events::Engine
     struct Init {};
     struct PostInit {};
 
-
     struct PreConfig {};
     struct Config {};
     struct PostConfig {};
 
-
     struct PreExecute {};
     struct Execute {};
     struct PostExecute {};
-
 
     struct PreTick {
         double deltaTime;
@@ -24,13 +21,11 @@ namespace Helena::Events::Engine
     struct Tick : PreTick {};
     struct PostTick : PreTick {};
 
-
     struct PreUpdate {
         double fixedTime;
     };
     struct Update : PreUpdate {};
     struct PostUpdate : PreUpdate {};
-
 
     struct PreRender {
         double alpha;
@@ -39,15 +34,37 @@ namespace Helena::Events::Engine
     struct Render : PreRender {};
     struct PostRender : PreRender {};
 
-
     struct PreFinalize {};
     struct Finalize {};
     struct PostFinalize {};
 
-
     struct PreShutdown {};
     struct Shutdown {};
     struct PostShutdown {};
+
+    template <typename>
+    struct PreRegisterSystem {};
+
+    template <typename>
+    struct PostRegisterSystem {};
+
+    template <typename>
+    struct PreRemoveSystem {};
+
+    template <typename>
+    struct PostRemoveSystem {};
+
+    template <typename>
+    struct PreRegisterComponent {};
+
+    template <typename>
+    struct PostRegisterComponent {};
+
+    template <typename>
+    struct PreRemoveComponent {};
+
+    template <typename>
+    struct PostRemoveComponent {};
 }
 
 #endif // HELENA_ENGINE_EVENTS_HPP
