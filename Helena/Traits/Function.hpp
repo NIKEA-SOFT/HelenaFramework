d38e67bc66524023306f358902fa7750217ac162
NSOFT <nsoft.sagaonline@gmail.com>
2024-07-23 09:17:28 +0300

    Refactoring Function traits

--- Helena/Traits/Function.hpp
+++ Helena/Traits/Function.hpp
@@ -7,11 +7,32 @@
 namespace Helena::Traits
 {
     template <typename Fn>
-    struct Function : Function<decltype(&Fn::operator())> {};
+    struct Function;
 
-    template <typename Ret, typename T, typename... Args>
-    struct Function<Ret(T::*)(Args...) const> : Identity<Ret(T::*)(Args...) const>, Arguments<Args...> {
-        using Class = T;
+    template <typename Fn>
+    struct Function : Function<decltype(&Fn::operator())>{};
+
+    template <typename Ret, typename... Args>
+    struct Function<Ret(Args...)> : Identity<Ret(Args...)>, Arguments<Args...> {
+        using Class = void;
+        using Return = Ret;
+    };
+
+    template <typename Ret, typename... Args>
+    struct Function<Ret(Args...) noexcept> : Identity<Ret(Args...) noexcept>, Arguments<Args...> {
+        using Class = void;
+        using Return = Ret;
+    };
+
+    template <typename Ret, typename... Args>
+    struct Function<Ret(*)(Args...)> : Identity<Ret(*)(Args...)>, Arguments<Args...> {
+        using Class = void;
+        using Return = Ret;
+    };
+
+    template <typename Ret, typename... Args>
+    struct Function<Ret(*)(Args...) noexcept> : Identity<Ret(*)(Args...) noexcept>, Arguments<Args...> {
+        using Class = void;
         using Return = Ret;
     };
 
@@ -21,9 +42,45 @@ namespace Helena::Traits
         using Return = Ret;
     };
 
-    template <typename Ret, typename... Args>
-    struct Function<Ret(*)(Args...)> : Identity<Ret(*)(Args...)>, Arguments<Args...> {
-        using Class = void;
+    template <typename Ret, typename T, typename... Args>
+    struct Function<Ret(T::*)(Args...) const> : Identity<Ret(T::*)(Args...) const>, Arguments<Args...> {
+        using Class = T;
+        using Return = Ret;
+    };
+
+    template <typename Ret, typename T, typename... Args>
+    struct Function<Ret(T::*)(Args...) const volatile> : Identity<Ret(T::*)(Args...) const volatile>, Arguments<Args...> {
+        using Class = T;
+        using Return = Ret;
+    };
+
+    template <typename Ret, typename T, typename... Args>
+    struct Function<Ret(T::*)(Args...) const noexcept> : Identity<Ret(T::*)(Args...) const noexcept>, Arguments<Args...> {
+        using Class = T;
+        using Return = Ret;
+    };
+
+    template <typename Ret, typename T, typename... Args>
+    struct Function<Ret(T::*)(Args...) const volatile noexcept> : Identity<Ret(T::*)(Args...) const volatile noexcept>, Arguments<Args...> {
+        using Class = T;
+        using Return = Ret;
+    };
+
+    template <typename Ret, typename T, typename... Args>
+    struct Function<Ret(T::*)(Args...) volatile> : Identity<Ret(T::*)(Args...) volatile>, Arguments<Args...> {
+        using Class = T;
+        using Return = Ret;
+    };
+
+    template <typename Ret, typename T, typename... Args>
+    struct Function<Ret(T::*)(Args...) volatile noexcept> : Identity<Ret(T::*)(Args...) volatile noexcept>, Arguments<Args...> {
+        using Class = T;
+        using Return = Ret;
+    };
+
+    template <typename Ret, typename T, typename... Args>
+    struct Function<Ret(T::*)(Args...) noexcept> : Identity<Ret(T::*)(Args...) noexcept>, Arguments<Args...> {
+        using Class = T;
         using Return = Ret;
     };
 }
